f29e3c4fc2f541bfb3d122c62cc7b2e2b176ecca
niccolas.cassel <niccolas.cassel@sequor.com.br>
2022-02-28 12:20:01 -0300

    Implmentação do Exercício do Módulo 14

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,94 @@
 /// <reference types="cypress" />
+const { faker } = require('@faker-js/faker');
+import contrato from '../contracts/usuarios.contract'
 
 describe('Testes da Funcionalidade Usuários', () => {
 
-    it('Deve validar contrato de usuários', () => {
-         //TODO: 
-    });
+     let token
+     let firstName = faker.name.firstName()
+     let lastName = faker.name.lastName()
+     let email = faker.internet.email(firstName, lastName)
+     let lastCreatedId
 
-    it('Deve listar usuários cadastrados', () => {
-         //TODO: 
-    });
+     before(() => {
+          cy.token('niccolas.cassel@teste.com.br', '123456').then(tkn => { token = tkn })
+     });
 
-    it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
-    });
+     it('Deve validar contrato de usuários', () => {
+          cy.request('usuarios').then(response => {
+               return contrato.validateAsync(response.body)
+          })
+     });
 
-    it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
-    });
+     it('Deve listar usuários cadastrados', () => {
+          cy.request({
+               method: 'GET',
+               url: 'usuarios'
+          }).then((response) => {
+               expect(response.status).to.equal(200)
+               expect(response.body).to.have.property('usuarios')
+               expect(response.duration).to.be.lessThan(20)
+          })
+     });
 
-    it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
-    });
+     it('Deve cadastrar um usuário com sucesso', () => {
 
-    it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
-    });
+          cy.cadastrarUsuario(
+               token,
+               `${firstName} ${lastName}`,
+               email,
+               "teste@teste.com",
+               "true"
+          ).then((response) => {
+               expect(response.status).to.equal(201)
+               expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+
+               lastCreatedId = response.body._id
+          })
+     });
+
+     it('Deve validar um usuário com email inválido', () => {
+
+          cy.cadastrarUsuario(
+               token,
+               `${firstName} ${lastName}`,
+               email,
+               "teste@teste.com",
+               "true"
+          ).then((response) => {
+               expect(response.status).to.equal(400)
+               expect(response.body.message).to.equal('Este email já está sendo usado')
+          })
+     });
+
+     it('Deve editar um usuário previamente cadastrado', () => {
+
+          cy.request({
+               method: 'PUT',
+               url: `usuarios/${lastCreatedId}`,
+               headers: { authorization: token },
+               body:
+               {
+                    "nome": `${firstName} ${lastName}`,
+                    "email": faker.internet.email(lastName, firstName),
+                    "password": "teste@teste.com",
+                    "administrador": "true"
+               }
+          }).then(response => {
+               expect(response.body.message).to.equal('Registro alterado com sucesso')
+          })
+     });
+
+     it('Deve deletar um usuário previamente cadastrado', () => {
+          
+          cy.request({
+               method: 'DELETE',
+               url: `usuarios/${lastCreatedId}`,
+               headers: { authorization: token }
+          }).then(response => {
+               expect(response.body.message).to.equal('Registro excluído com sucesso')
+          })
+     });
 
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -51,3 +51,18 @@ Cypress.Commands.add('token', (email, senha) => {
           failOnStatusCode: false
     })
  })
+
+ Cypress.Commands.add('cadastrarUsuario' , (token, nome, email, password, administrador) =>{
+    cy.request({
+        method: 'POST', 
+        url: 'usuarios',
+        headers: {authorization: token}, 
+        body: {
+            "nome": nome,
+            "email": email,
+            "password": password,
+            "administrador": administrador
+          }, 
+          failOnStatusCode: false
+    })
+ })
